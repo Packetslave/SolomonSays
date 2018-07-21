85f9f0b29dfe61a6df61dc1525769d794a56841f
Brian Landers <brian@packetslave.com>
2018-07-21 11:32:14 -0700

    Add duplicate detection using DynamoDB

--- solomon.py
+++ solomon.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
+import calendar
 import random
 import sys
+import time
+
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
 
 import twitter
 import xmltodict
@@ -9,7 +14,7 @@ import xmltodict
 sys.path.append(".")
 import twitter_config as config
 
-BLACKLIST = frozenset([
+BLACKLIST = set([
     "1:1",  # intro to the book
     "1:2",  # intro to the book
     "1:3",  # intro to the book
@@ -17,33 +22,71 @@ BLACKLIST = frozenset([
     "1:5",  # intro to the book
 ])
 
+BLACKLIST_SECS = 60 * 60 * 24 * 60
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(config.dynamodb_table)
+
+now = int(calendar.timegm(time.gmtime()))
+
+# Read the blacklist of verses we've posted recently
+
+response = table.scan(
+    FilterExpression=Attr('last_updated').gt(now - BLACKLIST_SECS)
+)
+for i in response['Items']:
+    print "Adding %s to blacklist (posted %.1f days ago)" % (
+        i["verse"],
+        (now - i["last_updated"]) / (60 * 60 * 24))
+    BLACKLIST.add(i['verse'])
+
+# Read the XML verses file
+
 doc = None
 with open('proverbs.xml') as fd:
     doc = xmltodict.parse(fd.read())
 
 root = doc['bible']['b']['c']
 
+# Keep track of number of verses per chapter and the total number
+
 chapters = {}
 verses = 0
 
 for chapter in root:
-    # Keep track of number of verses per chapter and the total number
     chapters[chapter["@n"]] = len(chapter["v"])
     verses += len(chapter["v"])
 
-# TODO: keep track of posted verses and don't select one that's been posted
-# in the past ${verses} days. That should eliminate duplicates.
+# Pick a verse to post, skip duplicates and blacklisted verses
 
-chapter_idx = random.randint(1, len(chapters))
-verse_idx = random.randint(1, len(root[chapter_idx-1]['v']))
+while True:
+    chapter_idx = random.randint(1, len(chapters))
+    verse_idx = random.randint(1, len(root[chapter_idx-1]['v']))
+    verse_key = "%s:%s" % (chapter_idx, verse_idx)
+    if verse_key not in BLACKLIST:
+        break
+
 verse = root[chapter_idx - 1]['v'][verse_idx - 1]['#text']
 
+# Post to twitter
+
 new_status = "%s (Proverbs %s:%s NIV)" % (verse, chapter_idx, verse_idx)
+print("status: %s" % new_status)
 
 twitter = twitter.Twitter(auth=twitter.OAuth(config.access_key,
-                                               config.access_secret,
-                                               config.consumer_key,
-                                               config.consumer_secret))
-
+                                             config.access_secret,
+                                             config.consumer_key,
+                                             config.consumer_secret))
 results = twitter.statuses.update(status=new_status)
-print("updated status: %s" % new_status)
+
+# Update DynamoDB with tinmestamp for the verse we just posted
+
+table.update_item(
+  Key={
+    "verse": "%s:%s" % (chapter_idx, verse_idx),
+  },
+  UpdateExpression='SET last_updated = :val1',
+  ExpressionAttributeValues={
+      ':val1': now,
+  }
+)
